943a99c1b63ebd10354ae729def7c0fa4f799271
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-09-06 22:55:18 +0530

    done

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,4 +1,35 @@
-async function getOrderById(id) {
+const order = require('../model/order')
+async function getOrderById() {
 
 }
+async function createOrder(req, res) {
+    try {
+        const { items, totalAmount, address, paymentId } = req.body;
+        if (!items || items.length === 0 || !address || !totalAmount) {
+            return res.status(404).json({ message: 'Invalid order Data' })
+        }
+        else{
+            const orders = new order.create({
+                user:req.user._id,
+                items,
+                totalAmount,
+                address,
+                paymentId   
+            })
+            await orders.save();
+         
+        }
+    }
+    catch (error) {
+        res.status(202).json({ message: 'Error occured', error })
+    }
+}
+async function getOrder() {
 
+}
+async function getOrderById() {
+
+}
+async function updateOrderLists() {
+
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { isAdmin } = require('../middleware/adminMiddleware');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.router()
-router.route('/myOrders').get(protect,getOrderById);
 router.route('/').post(protect, createOrder).get(protect,isAdmin,getOrder);
+router.route('/myOrders').get(protect,getOrderById);
 router.route(':id/status').put(protect,isAdmin,updateOrderLists )
 module.exports = router
