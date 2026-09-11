253e2de6674980e8bcbea7767e18cb8f6bd782bd
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-09-11 22:40:22 +0530

    backenddone

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,24 +1,28 @@
 const nodeMailer = require('nodemailer');
 require('dotenv').config();
 const sendEmail = async (to, subject, text) => {
-try{
-const transporter = nodeMailer.createTransport({
-   service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+    try {
+        const transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
 
-})
-const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: to,
-    subject: subject,
-    text: text
-};
-}
-catch(error){
-  console.log('Error sending email:', error);  
-}
+        })
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: to,
+            subject: subject,
+            text: text
+        };
+        const info =await transporter.sendMail(mailOptions);
+        console.log("Email Sent Successfully!", info.messageId)
+        return info;
+    }
+    catch (error) {
+        console.log('Error sending email:', error);
+    throw error
+    }
 }
 module.exports = sendEmail;
