566619fbc856fcea039dbfd092a4d821c436ce16
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-09-21 22:20:55 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 import './styles/global.css'
-
+ 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
