4ef51431a80a95c0acc233b7f0655c6069ad05ba
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-09-22 21:43:11 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+import store from './redux/store.js'
 import './styles/global.css'
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
   </StrictMode>
 )
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,1 +1,9 @@
-import {configureStore} from 'redux-' 
+import { configureStore } from "@reduxjs/toolkit"
+import cartReducer from '../redux/cartSlice'
+const store = configureStore({
+    reducer: {
+        cart: createReducerer
+    },
+})
+
+export default store
